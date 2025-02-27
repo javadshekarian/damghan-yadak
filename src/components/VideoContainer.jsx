35e899f8b189e9c8cb49439cc93e6b42a71a9948
javadshekarian <javadshekarian1378@gmail.com>
2025-02-27 18:34:02 +0330

    seven commit

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,16 +1,15 @@
 import '../styles/all-page.css';
+import videoFile from '../video/video.mp4';
 
 const VideoContainer = () => {
     return (
         <div className="video-container">
-            <video 
-                src="./video/video.mp4"
-                autoPlay={true}
-                loop
-                muted
-            ></video>
+            <video autoPlay={true} loop muted>
+                <source src={videoFile} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
         </div>
-    )
+    );
 };
 
 export default VideoContainer;
